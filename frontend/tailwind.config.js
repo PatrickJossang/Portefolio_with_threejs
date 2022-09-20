3488a401b01b385eb231ad7b00fc39153599e350
Patrick Jøssang <patrickjossang@gmail.com>
2022-09-20 17:46:15 +0200

    fixed navbar wide is not propper suportet yett

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      extend: {
+        Image: {
+          'NavBarIMG': "url('https://github.com/snorresovold/Teach_me/blob/main/frontend/public/world.png')",
+        },
+      },
+    },
   },
   plugins: [],
 }
