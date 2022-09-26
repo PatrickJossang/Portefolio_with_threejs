c6e095b3b281a7f54a2a4219a313d2b23aed4204
Patrick Jøssang <patrickj@iktfag.com>
2022-09-26 04:25:31 -0700

    threejs is ready

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,20 @@
+// index.jsx
+import css from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={css.scene}>
+
+    </div>
+  );
+}
+
+//Home.module.css
+.scene{
+    width:100vw;
+    height:100vh;
+}
+
+.canvas{
+    background: #000;
+}
